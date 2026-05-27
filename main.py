751e848c4320b246161c237a3a0e0b343af1af54
Hai Ngo <54173427+Devissam6@users.noreply.github.com>
2026-05-27 19:53:43 +0100

    feat: add file handling

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# print("Hello world")
 import argparse
+import json
+import os.path
+from pathlib import Path
+
+# ==================== HANDLING CLI ARGUMENTS ====================
 
 parser = argparse.ArgumentParser(
     prog="task-tracker",
@@ -12,3 +16,13 @@ parser.add_argument('context', help="Title or status of task")
 
 args = parser.parse_args()
 print(args)
+
+# ==================== HANDLING FILE IO ====================
+
+# if not os.path.exists('./tasks.json'):
+json_path = Path("tasks.json")
+if not json_path.exists():
+    json_path.touch()
+    print(f"{json_path} created.")
+else:
+    print(f"{json_path} already exists.")
