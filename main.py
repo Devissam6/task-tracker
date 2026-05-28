f6fd470cb8bf374e56972dfe24c9de15abd2d923
Hai Ngo <54173427+Devissam6@users.noreply.github.com>
2026-05-29 00:59:23 +0100

    add reading of JSON file

--- main.py
+++ main.py
@@ -15,14 +15,26 @@ parser.add_argument('id', help="ID number of existing task")
 parser.add_argument('context', help="Title or status of task")
 
 args = parser.parse_args()
-print(args)
+print(f"args: {args}")
 
 # ==================== HANDLING FILE IO ====================
 
 # if not os.path.exists('./tasks.json'):
 json_path = Path("tasks.json")
 if not json_path.exists():
-    json_path.touch()
+    json_path.write_text("{}")
     print(f"{json_path} created.")
 else:
     print(f"{json_path} already exists.")
+
+# ==================== DECODING JSON OBJECT ====================
+
+tasks = json.loads(json_path.read_text())
+print(f"Tasks: {tasks}; len: {len(tasks)}")
+
+# ==================== PROCESS ARGS ====================
+
+print(f"Action: {args.action}; type: {type(args.action)}")
+
+# test = input("what: ")
+# print(f"test: {test}")
